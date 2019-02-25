d80f65d93fa36b55b4e3e22a64dfb3b9ca96840d
colbymchenry <colbymchenry@gmail.com>
2019-02-24 20:59:45 -0500

    Added an authentication scaffold and worked on readme.

---
+++ database/migrations/2019_02_25_015535_create_transactions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('invoice_id');
+            $table->string('billing_agreement_id')->nullable();
+            $table->integer('user_id');
+            $table->integer('product_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
--- readme.md
+++ readme.md
@@ -1,3 +1,17 @@
-Install via composer\
-composer require paypal/rest-api-sdk-php:*
+**Install via composer**\
+composer require paypal/rest-api-sdk-php:*\
+**Make an authentication framework**\
+php artisan make:auth\
+**Create transaction migration**\
+php artisan make:model Transaction -m\
+**Set up the migration to look like this**\
+    `Schema::create('transactions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('invoice_id');
+            $table->string('billing_agreement_id')->nullable();
+            $table->integer('user_id');
+            $table->integer('product_id')->nullable();
+            $table->timestamps();
+        });`
+
 
